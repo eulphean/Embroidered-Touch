871892d842fb66dff0cd57697305610ac2e2b054
Amay Kataria <amay.kataria@gmail.com>
2021-04-27 20:14:37 -0500

    Add slider to adjust cut off values. Now hook up all the data into the UI elements

--- code_p5/src/Sensor.js
+++ code_p5/src/Sensor.js
@@ -5,6 +5,9 @@
 
 const activeSensorClass = 'indicator-active';
 
+const sliderMin = 0; 
+const sliderMax = 250;
+const sliderStep = 1; 
 class Sensor {
     constructor(sensorIdx, interfaceTrees) {
         this.parentTrees = interfaceTrees; 
@@ -30,12 +33,26 @@ class Sensor {
         this.baseVal = select(this.valueClass, baseNode);
         this.baseVal.html('BBB');
 
-        let cutoffValNode = this.parentTrees['cutoffval'];
-        this.cutoffVal = select(this.valueClass, cutoffValNode);
-        this.cutoffVal.html('CCC');
-
         let touchIndicatorNode = this.parentTrees['indicator'];
         this.touchIndicator = select(this.valueClass, touchIndicatorNode);
+
+
+        // Cutoff
+        let cutoffKnobNode = this.parentTrees['cutoffknob'];
+        this.cutoffSlider = select(this.valueClass, cutoffKnobNode);
+        this.cutoffSlider.attribute('min', sliderMin);
+        this.cutoffSlider.attribute('max', sliderMax);
+        this.cutoffSlider.attribute('value', 100); // TODO: Read this value from a JSON (save recurring values)
+        this.cutoffSlider.attribute('step', sliderStep);
+        this.cutoffSlider.elt.addEventListener('input', this.updateCutoffVal.bind(this));
+
+        let cutoffValNode = this.parentTrees['cutoffval'];
+        this.cutoffVal = select(this.valueClass, cutoffValNode);
+        this.cutoffVal.html(this.cutoffSlider.value());
+    }
+
+    updateCutoffVal() {
+        this.cutoffVal.html(this.cutoffSlider.value());
     }
 
     setData(sensorDataType, sensorVal) {
