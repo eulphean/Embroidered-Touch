664c018b70c1698b7e65752be103a4c61e368686
Amay Kataria <studio@amaykataria.com>
2021-06-16 13:48:40 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Fabric_Instrument/precache-manifest.0138801fe2825cb633b05ec721fe6d31.js"
+  "/Fabric_Instrument/precache-manifest.7df8453536027d19c28458edd9143189.js"
 );
 
 self.addEventListener('message', (event) => {
