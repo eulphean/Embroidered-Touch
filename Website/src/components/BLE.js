dd9f01a5619fc27d04c0c84f9dfbca564cd82680
Amay Kataria <studio@amaykataria.com>
2021-08-13 14:56:43 -0500

    Some testing changes

--- Website/src/components/BLE.js
+++ Website/src/components/BLE.js
@@ -79,6 +79,7 @@ class BLE {
   handleIncomingData(event) {
     let decoder = new TextDecoder('utf-8', {fatal: true});
     let data = decoder.decode(event.target.value);
+    console.log(data);
     //   // Clean the string with end of line characters. 
     data = data.replace(/\0[\s\S]*$/g,'');
     let chipsetIdx; let sensorDataType; let sensorData = []; 
@@ -89,7 +90,7 @@ class BLE {
 
     // // Debug: Uncomment for raw sensor data received from bluetooth. 
     // // console.log('Chipset Idx, Data type, sensor Data: ' + chipsetIdx + ", " + sensorDataType + ", " + sensorData);
-    sensorDataStore.setState(chipsetIdx, sensorDataType, sensorData); 
+    //sensorDataStore.setState(chipsetIdx, sensorDataType, sensorData); 
   }
 
   activateLife(chipIdx) {
@@ -127,6 +128,39 @@ class BLE {
     } else {
       return this.lifeData[1];
     }
+  }
+
+  Utf8ArrayToStr(array) {
+    var out, i, len, c;
+    var char2, char3;
+
+    out = "";
+    len = array.length;
+    i = 0;
+    while(i < len) {
+    c = array[i++];
+    switch(c >> 4)
+    { 
+      case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
+        // 0xxxxxxx
+        out += String.fromCharCode(c);
+        break;
+      case 12: case 13:
+        // 110x xxxx   10xx xxxx
+        char2 = array[i++];
+        out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
+        break;
+      case 14:
+        // 1110 xxxx  10xx xxxx  10xx xxxx
+        char2 = array[i++];
+        char3 = array[i++];
+        out += String.fromCharCode(((c & 0x0F) << 12) |
+                       ((char2 & 0x3F) << 6) |
+                       ((char3 & 0x3F) << 0));
+        break;
+    }
+    }
+    return out;
   }
 }
 
